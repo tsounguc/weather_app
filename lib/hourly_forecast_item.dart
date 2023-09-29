5165b62903e923feb3d25967a1319ea0c382be71
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-09-29 11:42:28 -0400

    completed weather app from udemay The Ultimate Dart & Flutter Course 2023

---
+++ lib/hourly_forecast_item.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HourlyForecastItem extends StatelessWidget {
+  final String time;
+  final String temperature;
+  final IconData icon;
+  const HourlyForecastItem({
+    super.key,
+    required this.time,
+    required this.temperature,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        padding: const EdgeInsets.all(8.0),
+        width: 100,
+        decoration: BoxDecoration(borderRadius: BorderRadius.circular(16)),
+        child: Column(
+          children: [
+            Text(
+              time,
+              style: const TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            const SizedBox(height: 8),
+            Icon(
+              icon,
+              size: 32,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              temperature,
+              style: const TextStyle(
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,1 @@
+const openWeatherAPIKey = 'ac6d28d37e9150d517b296cb947da78d';
